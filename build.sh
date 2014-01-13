a1278a1f333e18480631ef6d63196be210f0d5de
hekai <hwk256@gmail.com>
2014-01-13 22:32:44 +0800

    add neon optimization for arm.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Compiles fftw3 for Android
+# Make sure you have NDK_DIR defined in .bashrc or .bash_profile
+
+#NDK Version r9c  http://dl.google.com/android/ndk/android-ndk-r9c-linux-x86_64.tar.bz2
+NDK_DIR="/home/hekai/software/android-ndk-r9c"
+INSTALL_DIR="`pwd`/jni/fftw3"
+SRC_DIR="`pwd`/../fftw-3.3.3"
+
+cd $SRC_DIR
+
+export PATH="$NDK_DIR/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/:$PATH"
+export SYS_ROOT="$NDK_DIR/platforms/android-8/arch-arm/"
+export CC="arm-linux-androideabi-gcc --sysroot=$SYS_ROOT -march=armv7-a -mfloat-abi=softfp"
+export LD="arm-linux-androideabi-ld"
+export AR="arm-linux-androideabi-ar"
+export RANLIB="arm-linux-androideabi-ranlib"
+export STRIP="arm-linux-androideabi-strip"
+#export CFLAGS="-mfpu=neon -mfloat-abi=softfp"
+
+mkdir -p $INSTALL_DIR
+./configure --host=arm-eabi \
+	--prefix=$INSTALL_DIR \
+	LIBS="-lc -lgcc" \
+	--enable-float \
+	--enable-threads \
+#	--with-combined-threads \
+	--enable-neon
+
+make
+make install
+
+exit 0
